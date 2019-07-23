afe39015972ab295a9da25f6ccafb84e557613f5
Jose Labaki <jose.labaki@hotmail.com>
2019-07-23 12:46:05 +0300

    no-follow , css

--- app/assets/javascripts/on-page.js
+++ app/assets/javascripts/on-page.js
@@ -2,13 +2,21 @@ $( document ).on('turbolinks:load', function() {
     window.onload = $(function(){
         var current = window.location.pathname.split('?')[0];
         console.log(current)
-        $('.links-nav a').each(function(){
+        $('.projects-nav a , .static a , .contact a').each(function(){
             var $this = $(this);
             // if the current path is like this link, make it active
             if($this.attr('href').indexOf(current) !== -1){
                 $this.addClass('active');
             }});
 
-
+            $('.languages a').each(function(){
+                var $this = $(this);
+                // if the current path is like this link, make it active
+                if($this.attr('href').indexOf(current) == -1){
+                    $this.addClass('active-lang');
+                }});
 });
+
+
+
 });
