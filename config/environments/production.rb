3de7b1e530489130cf06409b5cb53bf1f2f0125d
joselabaki <jose.labaki@hotmail.com>
2020-02-09 18:14:08 +0100

    font-fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.action_controller.relative_url_root  = "/myrailsapp"
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
