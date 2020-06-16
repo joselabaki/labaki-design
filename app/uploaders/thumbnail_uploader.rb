1966427b8b5eda7f6d9207b2393509592cd3f3ad
joselabaki <jose.labaki@hotmail.com>
2020-06-16 15:54:26 +0300

    remove mime_types

--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -6,8 +6,7 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :fog
   # storage :fog
-  include CarrierWave::MimeTypes
-  process :set_content_type
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
